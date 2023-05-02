dcd2970bc164594f0d6f9845b0b8fb95be70a0b8
Afzal Ahmad <113641536+afzalahmad1@users.noreply.github.com>
2023-05-02 11:02:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+function calculateTotal() {
+  const prices = document.querySelectorAll('[data-ns-test="prices"]');
+  let total = 0;
+  prices.forEach(price => {
+    const priceValue = parseFloat(price.textContent.replace('$', ''));
+    if (!isNaN(priceValue)) {
+      total += priceValue;
+    }
+  });
+  const grandTotal = document.querySelector('[data-ns-test="grandTotal"]');
+  grandTotal.textContent = '$' + total.toFixed(2);
+}
+
+calculateTotal();
+
+// Listen for changes to the prices and recalculate the total
+const prices = document.querySelectorAll('[data-ns-test="prices"]');
+prices.forEach(price => {
+  const observer = new MutationObserver(mutations => {
+    calculateTotal();
+  });
+  observer.observe(price, { characterData: true, subtree: true });
+});
